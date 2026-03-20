d57924b21132a12ffd683001661d4d6a87179762
Ranjith9948 <129143511+Ranjith9948@users.noreply.github.com>
2026-03-20 09:05:05 +0530

    yes

--- run.py
+++ run.py
@@ -2,5 +2,7 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# -------------------- Optional Local Run -------------------- #
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
